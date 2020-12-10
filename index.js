9d9fa3e430e504bf3ea37aca26cd741ebe11201d
Mykol Benning <mykolbenning@Mykols-MBP.fios-router.home>
2020-12-09 23:17:36 -0500

    minor change

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const siteTwo = require('./siteTwo');
     try {
         await siteOne.getJeepPricesSiteOne(url);
         await siteTwo.getJeepPricesSiteTwo(url);
-        // console.log(await pUtils.getTitle(page));
-      
+        
     } catch (err) {
 
         console.error(err);
